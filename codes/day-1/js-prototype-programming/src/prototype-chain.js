894a9493f92da0502281c2aa4649a21a141ba2fe
siemens-c2c <joy_mon78@yahoo.co.in>
2020-11-09 12:07:57 +0530

    this keyword updated

---
+++ codes/day-1/js-prototype-programming/src/prototype-chain.js
@@ -0,0 +1,39 @@
+//3. constructor function
+Object.prototype.show = function () {
+    console.log('object show')
+}
+
+function person(fname, lname, location) {
+    this.firstName = fname;
+    this.lastName = lname;
+    this.location = location;
+    this.printInfo = function () {
+        return this.firstName + ', ' + this.lastName + ', ' + this.location;
+    };
+    // this.show = function () {
+    //     console.log('show')
+    // }
+}
+person.prototype.show = function () {
+    console.log('person show')
+}
+//console.log(person.prototype)
+var joydipFCObj = new person('joydip', 'mondal', 'bangalore');
+//console.log(joydipFCObj.__proto__)
+//{__proto__:null,firstName:'joydip', lastName:'mondal',location:'bangalore', prinInfo:function(){....}}
+//console.log(person.prototype===joydipFCObj.__proto__)
+joydipFCObj.show = function(){
+    console.log('joydip show')
+}
+
+
+console.log(Object.prototype.hasOwnProperty('show'));
+console.log(person.prototype.hasOwnProperty('show'))
+console.log(joydipFCObj.hasOwnProperty('show'))
+joydipFCObj.show()
+var sunilObj = new person('snuil', 'gupta', 'chennai');
+sunilObj.show()
+
+function doctor(){
+
+}
