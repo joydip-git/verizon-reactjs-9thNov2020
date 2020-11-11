2fe758b6667bbb6f4ca4d5a892bddb1d651964d6
siemens-c2c <joy_mon78@yahoo.co.in>
2020-11-11 10:08:24 +0530

    day-3 initial upload

---
+++ codes/day-3/let-keyword-js/let-keyword.js
@@ -0,0 +1,25 @@
+//console.log(x)//x=undefined
+//var x;
+// x = 10;
+// console.log(x)
+// var i;
+// for (i = 0; i < 2; i++) {
+//     //var x;
+//     x = 20;
+//     console.log(x)
+// }
+// console.log(x)
+
+//console.log(x)//x=undefined
+let x;
+x = 10;
+console.log(x)
+for (let i = 0; i < 2; i++) {
+    let x;
+    x = 20;
+    console.log(x)
+}
+//console.log(window.x)
+
+var y = 10;
+console.log(window.y)
