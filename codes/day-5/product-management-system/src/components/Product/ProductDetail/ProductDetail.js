b2e93d2487db98f410bd3793a1b87788bf390fd1
siemens-c2c <joy_mon78@yahoo.co.in>
2020-11-13 13:45:48 +0530

    pms prelunch update

--- codes/day-5/product-management-system/src/components/Product/ProductDetail/ProductDetail.js
+++ codes/day-5/product-management-system/src/components/Product/ProductDetail/ProductDetail.js
@@ -1,14 +1,60 @@
 import React from 'react'
-import { Redirect } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom'
+import { fetchProductByIdAsyncCallbackCreator } from '../../../redux/async-opertions/productAsyncCallbacks'
 
-const ProductDetail = () => {
-    return (
-        <div>
-            Product Detail
-            <br />
-            <button className='btn btn-primary'>Back To Products</button>
-        </div>
-    )
+const ProductDetail = (props) => {
+
+    const history = useHistory();
+    const params = useParams();
+    const id = parseInt(params.id)
+
+    const product = useSelector((appState) => appState.getSingleProduct.product)
+    const loading = useSelector((appState) => appState.getSingleProduct.loading)
+    const error = useSelector((appState) => appState.getSingleProduct.error)
+    const dispatch = useDispatch()
+
+    const callback = fetchProductByIdAsyncCallbackCreator(id)
+    dispatch(callback)
+
+    let design = null;
+    if (loading) {
+        design = <span>Loading....</span>;
+    } else if (error !== null) {
+        design = <span>{error}</span>;
+    } else if (product === null) {
+        design = <span>No Product Found with id: {id}</span>
+    } else {
+        design = (
+            <>
+                Name:&nbsp;<span>{product.productName}</span>
+                <br />
+                <button
+                    className='btn btn-primary'
+                    onClick={() => history.push('/products')}>
+                    Back To Products
+                </button>
+            </>
+        )
+    }
+    return design;
 }
-
+/*
+const mapPropsToStateProps = (appState) => {
+    return {
+        product: appState.getSingleProduct.product,
+        error: appState.getSingleProduct.error,
+        loading: appState.getSingleProduct.loading
+    }
+}
+const mapPropsToDispatch = (dispatch) => {
+    return {
+        getProductById: () => {
+           const callback = fetchProductByIdAsyncCallbackCreator(1)
+            dispatch(callback)
+        }
+    }
+}
+export default connect(mapPropsToStateProps, mapPropsToDispatch)(ProductDetail)
+*/
 export default ProductDetail
